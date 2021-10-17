e34a4f3fb45f5b4b1777b8f97ab6ffe61fbe97aa
NateTheGameDev <90973438+NateTheGameDev@users.noreply.github.com>
2021-10-17 18:05:31 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+var dogeTheAiImg = document.getElementById('dogeTheAiImage');
+function onDogeTheAiImgClick() {
+ window.open('https://natethegamedev.github.io/dogetheai/', 'idk', 'width=800','height=500') 
+}
+window.addEventListener('click',onDogeTheAiImgClick);
